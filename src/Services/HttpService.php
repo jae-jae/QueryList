bd58352117430da47b76cf9d63e7a72fd44e5626
Jaeger <JaegerCode@gmail.com>
2017-09-26 17:54:29 +0800

    update http plugin

--- src/Services/HttpService.php
+++ src/Services/HttpService.php
@@ -7,13 +7,28 @@
 
 namespace QL\Services;
 
+use GuzzleHttp\Cookie\CookieJar;
 use Jaeger\GHttp;
 use QL\QueryList;
 
 class HttpService
 {
+    protected static $cookieJar = null;
+
+    public static function getCookieJar()
+    {
+        if(self::$cookieJar == null)
+        {
+            self::$cookieJar = new CookieJar();
+        }
+        return self::$cookieJar;
+    }
+
     public static function get(QueryList $ql,$url,$args = null,$otherArgs = [])
     {
+        $otherArgs = array_merge([
+            'cookies' => self::getCookieJar()
+        ],$otherArgs);
         $html = GHttp::get($url,$args,$otherArgs);
         $ql->setHtml($html);
         return $ql;
@@ -21,6 +36,9 @@ class HttpService
 
     public static function post(QueryList $ql,$url,$args = null,$otherArgs = [])
     {
+        $otherArgs = array_merge([
+            'cookies' => self::getCookieJar()
+        ],$otherArgs);
         $html = GHttp::post($url,$args,$otherArgs);
         $ql->setHtml($html);
         return $ql;
